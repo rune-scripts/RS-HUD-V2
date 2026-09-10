9f55d30b2d46a854aed9634b5b8b68136a191798
RUNE SCRIPTS <132000521+runescripts-dev@users.noreply.github.com>
2026-09-10 17:41:46 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,32 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author "RUNE SCRIPTS"
+description "A simple, modern and optimized HUD displaying any data you want with a simple config."
+version "1.3.0"
+lua54 "yes"
+
+client_scripts {
+    'config.lua',
+    'client/cl-frameworks.lua',
+    'client/cl-main.lua'
+}
+
+ui_page 'web/index.html'
+
+files {
+    'web/index.html',
+    'web/assets/*.css',
+    'web/assets/*.js',
+    'stream/*.ytd',
+    'stream/*.gfx',
+    'stream/minimap.gfx',
+    'stream/int3232302352.gfx'
+}
+
+escrow_ignore {
+    'config.lua'
+}
+
+
+dependency '/assetpacks'
